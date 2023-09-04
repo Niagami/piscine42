17b77655577efdbc096313f665f148c3575b74b6
Niagami <jason.tste@gmail.com>
2023-09-04 10:18:08 +0200

    Add files via upload

--- Piscine/C04/ex02/ft_putnbr.c
+++ Piscine/C04/ex02/ft_putnbr.c
@@ -6,14 +6,14 @@
 /*   By: jteste <jteste@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 09:03:51 by jteste            #+#    #+#             */
-/*   Updated: 2023/09/02 10:42:27 by jteste           ###   ########.fr       */
+/*   Updated: 2023/09/03 12:27:49 by jteste           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include<unistd.h>
+#include <unistd.h>
 
-void	ft_putchar(char a)
+void	ft_putchar(char c)
 {
-	write(1, &a, 1);
+	write(1, &c, 1);
 }
 
 void	ft_putnbr(int nb)
@@ -22,30 +22,29 @@ void	ft_putnbr(int nb)
 	{
 		ft_putchar('-');
 		ft_putchar('2');
-		ft_putnbr(147483648);
+		nb = 147483648;
 	}
-	else if (nb < 0)
+	if (nb < 0)
 	{
 		ft_putchar('-');
 		nb = -nb;
 	}
-	else if (nb >= 10)
+	if (nb >= 10)
 	{
 		ft_putnbr(nb / 10);
-		ft_putchar(nb % 10 + '0');
+		ft_putnbr(nb % 10);
 	}
 	else
 	{
-		ft_putnbr(nb + '0');
+		ft_putchar(nb + '0');
 	}
 }
-
 /*
 int	main(void)
 {
 	int	nb;
 
-	nb = 1234;
+	nb = -72312345;
 	ft_putnbr(nb);
 	ft_putchar('\n');
 	return (0);
--- Piscine/C04/ex03/ft_atoi.c
+++ Piscine/C04/ex03/ft_atoi.c
@@ -6,33 +6,46 @@
 /*   By: jteste <jteste@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 10:48:00 by jteste            #+#    #+#             */
-/*   Updated: 2023/09/02 11:33:58 by jteste           ###   ########.fr       */
+/*   Updated: 2023/09/02 13:30:32 by jteste           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_atoi(char *str)
 {
 	int	i;
-	int	j;
-	int	sign;
+	int	resultat;
+	int	signe;
 
 	i = 0;
-	j = 0;
-	sign = 1;
+	resultat = 0;
+	signe = 1;
 	while (str[i] == ' ' || str[i] == '\f' || str[i] == '\n'
 		|| str[i] == '\r' || str[i] == '\t' || str[i] == '\v')
 		i++;
-	if (str[i] == '-')
+	while (str[i] == '-' || str[i] == '+')
 	{
-		sign = -sign;
+		if (str[i] == '-')
+		{
+			signe = -signe;
+		}
 		i++;
 	}
-	else if (str[i] == '+')
-		i++;
 	while (str[i] >= '0' && str[i] <= '9')
 	{
-		j = j * 10 + (str[i] - '0');
+		resultat = resultat * 10 + (str[i] - '0');
 		i++;
 	}
-	return (j * sign);
+	return (resultat * signe);
 }
+/*
+#include<stdio.h>
+int main()
+{
+	char	str[] = "        +----+---+---+---420420420dhfghgckas";
+	printf("%s\n", str);
+	printf("%d\n", ft_atoi(str));
+	
+
+	return 0;
+}
+*/
